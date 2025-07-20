775f8a9ed5ff471ec86a5d65428d78d2fa736241
Minh Triet <phoenixmt1705@gmail.com>
2025-07-20 17:02:46 +0700

    change database from local MySql to Postgres

--- backend/UrlShortener.Infrastructure/Migrations/20250720095919_InitialCreatePostgre.cs
+++ backend/UrlShortener.Infrastructure/Migrations/20250720095919_InitialCreatePostgre.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
 namespace UrlShortener.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialCreatePostgre : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,11 +17,11 @@ namespace UrlShortener.Infrastructure.Migrations
                 columns: table => new
                 {
                     Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    LongUrl = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
-                    ShortCode = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
-                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    ClickCount = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LongUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    ShortCode = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ClickCount = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
