587090868de175e288adea9f9684c80f99edf059
Minh Triet <phoenixmt1705@gmail.com>
2025-07-22 10:48:29 +0700

    modified to catch and validate the input user url

--- backend/UrlShortener.Application/Services/UrlShorteningService.cs
+++ backend/UrlShortener.Application/Services/UrlShorteningService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using UrlShortener.Application.Interfaces;
 using UrlShortener.Domain.Entities;
 
@@ -7,14 +8,20 @@ namespace UrlShortener.Application.Services
     public class UrlShorteningService : IUrlShorteningService
     {
         private readonly IApplicationDbContext _context;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public UrlShorteningService(IApplicationDbContext context)
+        public UrlShorteningService(IApplicationDbContext context, IHttpClientFactory httpClientFactory)
         {
             _context = context;
+            _httpClientFactory = httpClientFactory;
         }
 
         public async Task<ShortenedUrl> GenerateShortUrlAsync(string longUrl)
         {
+            if (!await IsUrlReachable(longUrl))
+            {
+                throw new ArgumentException("The provided URL is not reachable or does not exist.");
+            }
             string shortCode;
             while (true)
             {
@@ -43,6 +50,28 @@ namespace UrlShortener.Application.Services
             var random = new Random();
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private async Task<bool> IsUrlReachable (string longurl)
+        {
+            if (!Uri.TryCreate(longurl, UriKind.Absolute, out var validatedUri))
+            {
+                return false;
+            }
+
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                client.Timeout = TimeSpan.FromSeconds(5);
+
+                var request = new HttpRequestMessage(HttpMethod.Head, validatedUri);
+                var response = await client.SendAsync(request);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
             //Redirect(Jukain)
