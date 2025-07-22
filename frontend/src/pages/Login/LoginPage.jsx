44295cf238d2506050c1c11c40b58d1e890c6fed
Minh Triet <phoenixmt1705@gmail.com>
2025-07-22 15:32:42 +0700

    finish Login and Register function and api for frontend

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button/Button";
 import InputForm from "../../components/InputForm/InputForm";
 import Box from "../../components/Box/Box";
 import "./LoginPage.css";
+import { Link, useNavigate } from "react-router-dom";
+import { loginApi } from "../../utils/api";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setError("");
+    try {
+      const data = await loginApi(username, password);
+      localStorage.setItem("jwtToken", data.token);
+      alert("Login successful!");
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+      console.error("Login failed: ", error);
+    }
+    console.log("Logging in with:", { username, password });
+  };
   return (
     <div className="container">
       <Box>
-        <div className="form-container">
+        <form onSubmit={handleLogin} className="form-container">
           <div className="group">
-            <label className="text-title">Username</label>
-            <InputForm />
+            <label htmlFor="username" className="text-title">
+              Username
+            </label>
+            <InputForm
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
           <div className="group">
-            <label className="text-title">Password</label>
-            <InputForm />
+            <label htmlFor="password" className="text-title">
+              Password
+            </label>
+            <InputForm
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <div className="action-group">
             <Button>Login</Button>
             <p>
-              Don't have an account yet? <a href="/register">Register</a>
+              Don't have an account yet? <Link to="/register">Register</Link>
             </p>
           </div>
-        </div>
+        </form>
       </Box>
     </div>
   );
